b9df08742a4505ba4fda72965d19c7a0334b9a20
sasha <sashagalois@gmail.com>
2015-03-13 12:44:39 -0700

    dynamic route

--- klax.py
+++ klax.py
@@ -1,9 +1,28 @@
+# [x] initial application
+# [ ] added dynamic route
+# [ ] integrated templates
+# [ ] integrated templates 2
+# [ ] bootstrap-template integration: user.html
+# [ ] template: generalized an app template for inheritance
+# [ ] templates: error pages
+# [ ] dynamic URLs, static files
+# [ ] integrated flask-moment: JS time library
+# [ ] web forms with flask-wtf (4a)
+# [ ] redirects and user sessions (4b)
+# [ ] message flashing (4c)
+
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 
 if __name__ == '__main__':
     app.run(debug = True)
