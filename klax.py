f8d47039a070a4805650fc87c15a7780c835925a
sasha <sashagalois@gmail.com>
2015-03-13 14:05:49 -0700

    dynamic URLs; static files using favicon.ico

--- klax.py
+++ klax.py
@@ -4,15 +4,15 @@
 # [x] integrated an extension: flask-script 
 # [x] bootstrap-template integration: user.html
 # [x] template: generalized an app template for inheritance
-# [ ] templates: error pages
-# [ ] dynamic URLs, static files
+# [x] templates: error pages
+# [x] dynamic URLs, static files using favicons
 # [ ] integrated flask-moment: JS time library
 # [ ] web forms with flask-wtf (4a)
 # [ ] redirects and user sessions (4b)
 # [ ] message flashing (4c)
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 
