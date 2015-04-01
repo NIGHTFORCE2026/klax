30262750933a25c1cc69663bdb3cdff56ed8cd7f
sasha <sashagalois@gmail.com>
2015-03-31 18:25:18 -0700

    Database use in the application (5b)

--- klax.py
+++ klax.py
@@ -15,7 +15,8 @@
 #       form handling in view functions
 # [x] redirects and user sessions (4b)
 # [x] message flashing (4c)
-# [ ] Database models with Flask-SQLAlchemy (5a)
+# [x] Database models with Flask-SQLAlchemy (5a)
+# [x] Database use in the application (5b)
 
 import os
 from flask import Flask, render_template, url_for, session, redirect, flash
@@ -26,33 +27,21 @@ from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required 
-
-# import the extension
 from flask.ext.sqlalchemy import SQLAlchemy
 
-# define a directory reference where the db should reside
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# set configuration parameters for the db location and auto commits
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shesanuptownmodel'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-# integrate the extension
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 
-# define the db schema represented in UML
-#   associate tables to classes 
-#   associate columns to attributes
-#   configure primary keys
-#   associate tables via foreign key
-#   associate objects 
-#   configure column types
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -72,8 +61,6 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-
-
 class NameForm(Form):
     name = StringField('What is your name?', validators = [Required()])
     submit = SubmitField('Submit')
@@ -90,15 +77,22 @@ def internal_server_error(e):
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     a_form = NameForm()
-    # flash a message if name in session different from name in input
+    # field has data, data is validated, form submitted
     if a_form.validate_on_submit():
-        prior_name = session.get('name')
-        if prior_name is not None and prior_name != a_form.name.data:
-            flash('Looks like you have changed your name!')
+        # check User table for username matching form input 
+        user = User.query.filter_by(username=a_form.name.data).first()
+        if user is None:
+            # write form input to a table row
+            user = User(username=a_form.name.data)
+            db.session.add(user)
+            # set a session variable to use in a cusomized template message
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name']= a_form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form = a_form, name = session.get('name'))
-
+    return render_template('index.html', form=a_form, name=session.get('name'), 
+            known=session.get('known', False)) 
 
 if __name__ == '__main__':
     manager.run()
