26222c37b9e5944714ca67a1f438bb40eecde45f
sasha <sashagalois@gmail.com>
2015-07-14 15:09:24 -0700

    blog posts on profile pages (11b)

--- app/main/views.py
+++ app/main/views.py
@@ -20,12 +20,13 @@ def index():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', form=form, posts=posts)
 
+
+# show list of the user's blog posts on their profile page
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        abort(404)
-    return render_template('user.html', user=user)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
