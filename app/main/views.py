38df1aaa7d747e996a58163432d37c0aecd9ab52
sasha <sashagalois@gmail.com>
2015-07-20 21:55:40 -0700

    pagination: render paginated data on a page

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template, redirect, url_for, abort, flash, request,\
+    current_app
 from flask.ext.login import login_required, current_user
 from . import main 
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -17,8 +18,14 @@ def index():
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    # new shit ---
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+            page, per_page=current_app.config['KLAX_POSTS_PER_PAGE'], 
+            error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts,
+            pagination=pagination)
 
 
 # show list of the user's blog posts on their profile page
