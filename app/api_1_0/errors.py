35ecb27718f3e9833ba8329b3a755d112aec4c0a
sasha <sashagalois@gmail.com>
2015-10-14 15:10:40 -0700

    error handling: added message parameter to json

--- app/api_1_0/errors.py
+++ app/api_1_0/errors.py
@@ -1,17 +1,18 @@
 from flask import jsonify
 from . import api
 
-def bad_request():
-    response = jsonify({'error': 'bad request'})
+# use these error handlers in the api's view functions
+def bad_request(message):
+    response = jsonify({'error': 'bad request', 'message': message})
     response.status_code = 400
     return response
 
-def unauthorized():
-    response = jsonify({'error': 'unauthorized'})
+def unauthorized(message):
+    response = jsonify({'error': 'unauthorized', 'message': message})
     response.status_code = 401
     return response
 
-def forbidden():
-    response = jsonify({'error': 'forbidden'})
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
     response.status_code = 403
     return response
