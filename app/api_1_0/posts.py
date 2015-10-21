329017230f2842ed0f1378ff1549f88a4310fdce
sasha <sashagalois@gmail.com>
2015-10-21 14:16:43 -0700

    resource endpoints: PUT handler for posts

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -29,3 +29,16 @@ def new_post():
     return jsonify(post.to_json()), 201, \
     { 'Location': url_for('api.get_post', id=post.id, _external=True) }
 
+@api.route('/posts/<int:id>', methods=['PUT'])
+@permission_required(Permission.WRITE_ARTICLES)
+def edit_post(id):
+    """ edit a post object attribute from json in client request
+    and return a json response containing the post """
+    post = Post.query.get_or_404(id)
+    if g.current_user != post.author and \
+            not g.current_user.can(Permission.ADMINISTER):
+        return forbidden('Insufficient permissions')
+    post.body = request.json.get('body', post.body)
+    db.session.add(post)
+    return jsonify(post.to_json())
+
