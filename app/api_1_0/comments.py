2ff6b8fff62c9b0b7958961fecbe37884b901ca9
sasha <sashagalois@gmail.com>
2015-10-21 18:10:47 -0700

    resource endpoints: GET handler for paginated collection of follwed user posts

---
+++ app/api_1_0/comments.py
@@ -0,0 +1,2 @@
+from flask import jsonify, current_app, url_for
+from . import api
--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -28,8 +28,23 @@ def get_user_posts(id):
         'count': pagination.total
     })
 
-
-
 @api.route('/users/<int:id>/timeline/')
-def get_user_followed_posts():
-    pass
+def get_user_followed_posts(id):
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followed_posts.paginate(page, 
+            per_page=current_app.config['KLAX_POSTS_PER_PAGE'],
+            error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        prev = url_for('api.get_posts', page=page+1, _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
