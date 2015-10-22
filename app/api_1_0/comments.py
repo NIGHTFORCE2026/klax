007fbea61f3ce34836690750aebf291ad1ff32ad
sasha <sashagalois@gmail.com>
2015-10-21 18:46:37 -0700

    resource endpoints: GET handle for paginated collection of comments on a post

--- app/api_1_0/comments.py
+++ app/api_1_0/comments.py
@@ -28,3 +28,24 @@ def get_comments():
         'next': next,
         'count': pagination.total
     })
+
+@api.route('/posts/<int:id>/comments/')
+def get_post_comments(id):
+    post = Post.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=current_app.config['KLAX_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_comments', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_comments', page=page+1, _external=True)
+    return jsonify({
+        'posts': [comment.to_json() for comment in comments],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
